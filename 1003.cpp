d7f13aac45dca35170a79e244aff5ccc8fe3017b
misule0423 <misule0423@gmail.com>
2016-09-26 22:15:09 +0900

    Update 1003.cpp

--- 1003.cpp
+++ 1003.cpp
@@ -2,45 +2,96 @@
 //  main.cpp
 //  1003
 //
-//  Created by 이민석 on 2016. 9. 1..
+//  Created by 이민석 on 2016. 9. 26..
 //  Copyright © 2016년 Roop. All rights reserved.
 //
 
 #include <iostream>
+#include <cstdio>
 using namespace std;
 
-int z=0, o=0;
-
-int fibonacci(int n) {
-    if (n==0) {
-        z++;
-        return 0;
-    } else if (n==1) {
-        o++;
-        return 1;
-    } else {
-        return fibonacci(n-1) + fibonacci(n-2);
-    }
-}
+int fibo[41];
+int fibo_zero[41];
+int fibo_one[41];
 
 int main(int argc, const char * argv[]) {
+
+    int tc;
     
-    int t;
+    cin >> tc;
     
-    cin >> t;
+    fibo[0] = 1;
+    fibo[1] = 1;
     
-    while (t--) {
+    fibo_zero[0] = 1;
+    fibo_zero[1] = 0;
+    fibo_one[0] = 0;
+    fibo_one[1] = 1;
+    
+    while(tc--){
+    
+        int N;
         
-        int n;
+        cin >> N;
         
-        cin >> n;
+        for (int i=2; i<=N; i++) {
+            fibo[i] = fibo[i-1] + fibo[i-2];
+            fibo_zero[i] = fibo_zero[i-1] + fibo_zero[i-2];
+            fibo_one[i] = fibo_one[i-1] + fibo_one[i-2];
+        }
         
-        fibonacci(n);
+        cout << fibo_zero[N] << " " << fibo_one[N] << endl;
         
-        cout << z << " " << o << endl;
-        
-        z=o=0;
     }
+    
     
     return 0;
 }
+
+
+// //
+// //  main.cpp
+// //  1003
+// //
+// //  Created by 이민석 on 2016. 9. 1..
+// //  Copyright © 2016년 Roop. All rights reserved.
+// //
+
+// #include <iostream>
+// using namespace std;
+
+// int z=0, o=0;
+
+// int fibonacci(int n) {
+//     if (n==0) {
+//         z++;
+//         return 0;
+//     } else if (n==1) {
+//         o++;
+//         return 1;
+//     } else {
+//         return fibonacci(n-1) + fibonacci(n-2);
+//     }
+// }
+
+// int main(int argc, const char * argv[]) {
+    
+//     int t;
+    
+//     cin >> t;
+    
+//     while (t--) {
+        
+//         int n;
+        
+//         cin >> n;
+        
+//         fibonacci(n);
+        
+//         cout << z << " " << o << endl;
+        
+//         z=o=0;
+//     }
+    
+//     return 0;
+// }
